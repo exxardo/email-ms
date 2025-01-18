7cd7b0754ecf445a35c8095c2e876e3be985f5a5
exxardo <eduardoramirodematos@gmail.com>
2025-01-18 20:53:21 -0300

    Comunicação do producer, com o broker e com o consumer

--- src/main/java/com/ms/email_ms/dtos/EmailDto.java
+++ src/main/java/com/ms/email_ms/dtos/EmailDto.java
@@ -2,6 +2,6 @@ package com.ms.email_ms.dtos;
 
 import java.util.UUID;
 
-public record EmailDto(UUID id, String emailUser, String subject, String message) {
+public record EmailDto(UUID userId, String emailUser, String subject, String message) {
 
 }
